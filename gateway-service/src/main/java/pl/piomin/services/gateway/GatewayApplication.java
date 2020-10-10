4d8dd49e50553929e9cd438021399fa23a8ef643
GarySu <Gary.Su@hearst.com>
2020-10-10 16:35:43 -0400

    Adding nginx ingress setup; swagger working

--- gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -1,8 +1,10 @@
 package pl.piomin.services.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,6 +14,11 @@ public class GatewayApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public InMemoryHttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 	
 }
